92c0dd8362b85495fc9d825fba432ae9705c0180
Clarissa Xu <clarissa_xu24@outlook.com>
2017-08-08 13:24:03 +0000

    Update

--- ruby-strings.rb
+++ ruby-strings.rb
@@ -1,13 +1,13 @@
 puts "What is your name?"
-guest_name = gets.chomp()
+guest_name = gets.chomp
 puts "What is the party name?"
-party_name = gets.chomp()
+party_name = gets.chomp
 puts "When is the party?"
-date = gets.chomp()
+date = gets.chomp
 puts "What time is the party?"
-time= gets.chomp()
+time= gets.chomp
 puts "Who is the host?"
-host_name= gets.chomp()
+host_name= gets.chomp
 
 puts "Dear #{guest_name},"
 puts "You are cordially invited to #{party_name} on #{date} at #{time}. Please RSVP no later than tomorrow."
